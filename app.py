66c95f90b0f1d4657d9c33805396ccdc4d09f995
“amyvong” <coreynielsen@Corey-Nielsens-MacBook-Air-908.local>
2020-08-26 13:28:27 -0700

    edited app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import numpy as np
-import pandas as pd
 import datetime as dt
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
-from sqlalchemy import create_engine, inspect, func
+from sqlalchemy import create_engine, func
 from sqlalchemy.orm import Session
 from flask import Flask, jsonify
 
@@ -12,7 +11,7 @@ Base = automap_base()
 Base.prepare(engine, reflect=True)
 measurement = Base.classes.measurement
 station=Base.classes.station
-session=Session(i8,engine)
+
 
 app=Flask(__name__)
 
@@ -27,56 +26,59 @@ def welcome():
         f"/api/v1.0/temp/start/end"
     )
 
+
+
 @app.route("/api/v1.0/stations")
 def stations():
-session = Session(engine)
-session.query(station.station,station.id)).all()
-session.close()
+    session = Session(engine)
+    results = session.query(station.station).all()
+    session.close()
+    stations = list(np.ravel(results))
 
-stations=[]
-for name, id in stations:
-    stationdict = {}
-    station['name'] = station[0]
-    station['id'] = station[1]
-    stations.append(stationdict)
-
-return jsonify(stations)
+    return jsonify(stations)
 
 
+@app.route("/api/v1.0/temp/start/end")
+def startend():
+    session = Session(engine)
+    sel = [measurement.date,measurement.prcp]
+    lastdate=session.query(measurement.date).order_by(measurement.date.desc()).first()
+    querydate = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    lastdate = dt.datetime.strptime(lastdate[0], '%Y-%m-%d')
+    querydate = dt.date(lastdate.year -1, lastdate.month, lastdate.day)
+    prcpquery = session.query(*sel).filter(measurement.date >= querydate).all()
+    session.close()
+
+    startend = []
+    for rain in prcpquery:
+        raindict = {}
+        raindict['date'] = rain[0]
+        raindict['amount'] = rain[1]
+        startend.append(raindict)
+
+    return jsonify(startend)
 
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-session = Session(engine)
-sel = [measurement.date,measurement.prcp]
-prcpquery = session.query(*sel).filter(measurement.date >= querydate).all()
+    session = Session(engine)
+    date_str = "12"
+    prcpresults=session.query(measurement.prcp).\
+        filter(func.strftime("%m", measurement.prcp) == date_str).all() 
+    session.close()
+    precipitation = list(np.ravel(prcpresults))
 
-session.close()
-
-precipitation = []
-for date, amount in prcpquery:
-    raindict = {}
-    raindict['date'] = rain[0]
-    raindict['amount'] = rain[1]
-    precipitation.append(raindict)
-
-return jsonify(precipitation)
+    return jsonify(precipitation)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-session = Session(engine)
-session.query(func.min(measurement.tobs),func.max(measurement.tobs),func.avg(measurement.tobs))
-session.close()
+    session = Session(engine)
+    tobsresults = session.query(measurement.tobs).all()
+    session.close()
 
-tobs=[]
-for minimum, maximum, average in tobs:
-    tobsdict = {}
-    tobsdict['min'] = tobsdict[0]
-    tobsdict['max'] = tobsdict[1]
-    tobsdict['avg'] = tobsdict[2]
-    tobs.append(tobsdict)
+    tobslist = list(np.ravel(tobsresults))
 
-return jsonify(tobs)
+    return jsonify(tobslist)
 
 
 
